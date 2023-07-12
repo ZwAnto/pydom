22b8cdcd4b944b6e44c0e3b20b68fecc3ea5e88e
zwanto <antoine.hamon@protonmail.com>
2023-07-12 21:41:25 +0200

    edit CORS

--- pydom/api.py
+++ pydom/api.py
@@ -18,10 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"]
 )
 
 @app.on_event("startup")
